b86a0a6b99d34efa826b240232088ba9932c369e
Robert Timm <mail@rtti.de>
2020-12-30 22:15:19 +0100

    add attendee, query, option, choice model + admin

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 
-from .models import Item
+from .models import Query, Option, Attendee, Choice
 
 
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    list_filter = ['name']
+class OptionInline(admin.StackedInline):
+    model = Option
+    extra = 1
+
+
+@admin.register(Query)
+class QueryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    inlines = [OptionInline]
+
+
+@admin.register(Attendee)
+class AttendeeAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
-admin.site.register(Item, ItemAdmin)
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('attendee', 'query', 'option', 'status')
+    list_display_links = ('attendee', 'query', 'option', 'status')
+    list_filter = ('attendee', 'option__query')
+
+    def query(self, obj):
+        return obj.option.query
--- app/models.py
+++ app/models.py
@@ -6,3 +6,65 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Query(models.Model):
+    name = models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Queries"
+
+
+class Option(models.Model):
+    begin_date = models.DateField()
+    begin_time = models.TimeField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+
+    def __str__(self):
+        result = str(self.begin_date)
+        if self.begin_time:
+            result += ' ' + str(self.begin_time.strftime('%H:%M'))
+        if self.end_date or self.end_time:
+            result += ' -'
+            if self.end_date:
+                result += ' ' + str(self.end_date)
+            if self.end_time:
+                result += ' ' + str(self.end_time.strftime('%H:%M'))
+        return '%s (%s)' % (result, str(self.query))
+
+    class Meta:
+        ordering = ['id']
+
+
+class Attendee(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Status(models.TextChoices):
+    YES = 'Y', 'Yes'
+    NO = 'N', 'No'
+    MAYBE = 'M', 'Maybe'
+
+
+class Choice(models.Model):
+    attendee = models.ForeignKey(Attendee, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=Status.choices)
+
+    def __str__(self):
+        return (self.attendee.name + '\'s choice for "' +
+                self.option.query.name + '": ' +
+                str(self.option.begin_date) + ' ' + str(self.status) + '')
+
+    class Meta:
+        constraints = [
+                models.UniqueConstraint(
+                    fields=['attendee', 'option'], name='unique_choice')]
