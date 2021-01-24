140cd0e07e9a9223cfe8ac46a77f73cead59b301
Robert Timm <mail@rtti.de>
2021-01-24 21:21:39 +0100

    switch nginx forground, watch background

--- nginx/nginx-start-watch-certs.sh
+++ nginx/nginx-start-watch-certs.sh
@@ -1,10 +1,22 @@
-#/bin/bash
+#/bin/sh
 
-nginx -g "daemon off;" &
+set -e
+# set -x
 
-while true; do 
-  inotifywait -e create /etc/letsencrypt/live/poodle.rtti.de/; 
-  echo "reloading nginx"; 
-  nginx -s reload; 
-  sleep 1
-done
+SLEEP_BEFORE_RELOAD=10
+
+watch_certs_and_reload() {
+  while true; do
+    inotifywait -e create /etc/letsencrypt/live/poodle.rtti.de/;
+    echo "cert change detected, waiting $SLEEP_BEFORE_RELOAD seconds before reload";
+    sleep $SLEEP_BEFORE_RELOAD
+    echo "reloading nginx";
+    nginx -s reload;
+    echo "done";
+  done
+}
+
+watch_certs_and_reload &
+
+nginx -g "daemon off;"
+
